3e7d1fc78585f3ec8ba8e5cb1aaa415305c0655f
Rolex <drolewski@gmail.com>
2024-01-31 21:44:15 +0100

    WB-1 Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    /**
+     * Set base path. This is usually the slug of your repository.
+     *
+     * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+     */
+    basePath: "",
+
+    /**
+     * Disable server-based image optimization. Next.js does not support
+     * dynamic features with static exports.
+     *
+     * @see https://nextjs.org/docs/pages/api-reference/components/image#unoptimized
+     */
+    images: {
+        unoptimized: true,
+    },
+};
 
 export default nextConfig;
