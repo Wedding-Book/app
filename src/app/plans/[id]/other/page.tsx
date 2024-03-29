717e27e13d3b7d6d2e8269aa6a05f22a70a65ce7
Rolex <drolewski@gmail.com>
2024-03-29 20:55:37 +0100

    WB-20 Other

--- src/app/plans/[id]/other/page.tsx
+++ src/app/plans/[id]/other/page.tsx
@@ -1,11 +1,12 @@
 import serverSessionChecker from '@/components/protected/serverSessionChecker';
 import {unavailablePlanRedirector} from '@/components/plans/security/unavailablePlanRedirector';
+import Other from '@/components/plans/other/other';
 
 const OthersPage = async ({params}: { params: { id: string } }) => {
   await serverSessionChecker({noSessionPath: '/login'});
   await unavailablePlanRedirector(params.id);
 
-  return <>Other page</>
+  return <Other planId={params.id}/>
 }
 
 export default OthersPage;
---
+++ src/components/plans/other/other.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+type OtherProps = {
+  planId: string,
+}
+
+const Other = ({planId}: OtherProps) => {
+  return <>Client Other</>
+}
+
+export default Other;
